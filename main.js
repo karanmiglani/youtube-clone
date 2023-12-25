afd5f46baeae541e2f8bab3d0f5bcca24f81a985
karanmiglani <karanmiglani03@gmail.com>
2023-12-25 11:49:52 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('.menu-icon')
+const sideBar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+
+menuIcon.onclick = function(){
+    sideBar.classList.toggle('small-sidebar')
+    container.classList.toggle('large-container');
+}
